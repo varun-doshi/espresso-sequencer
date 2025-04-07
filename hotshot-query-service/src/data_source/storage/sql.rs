523bca4a2701e05d2ef820428fcf12a947c4ddd1
Abdul Basit <45506001+imabdulbasit@users.noreply.github.com>
2025-04-08 01:16:35 +0500

    deserialize vid share as option in types migration (#2940)

--- hotshot-query-service/src/data_source/storage/sql.rs
+++ hotshot-query-service/src/data_source/storage/sql.rs
@@ -884,13 +884,18 @@ impl<Types: NodeType> MigrateTypes<Types> for SqlStorage {
 
                 // TODO (abdul): revisit after V1 VID has common field
                 let vid_common_bytes: Vec<u8> = row.try_get("vid_common")?;
-                let vid_share_bytes: Vec<u8> = row.try_get("vid_share")?;
+                let vid_share_bytes: Option<Vec<u8>> = row.try_get("vid_share")?;
 
-                let vid_share: ADVZShare = bincode::deserialize(&vid_share_bytes)
-                    .context("failed to deserialize vid_share")?;
+                let mut new_vid_share_bytes = None;
 
-                let new_vid_share_bytes = bincode::serialize(&VidShare::V0(vid_share))
-                    .context("failed to serialize vid_share")?;
+                if let Some(vid_share_bytes) = vid_share_bytes {
+                    let vid_share: ADVZShare = bincode::deserialize(&vid_share_bytes)
+                        .context("failed to deserialize vid_share")?;
+                    new_vid_share_bytes = Some(
+                        bincode::serialize(&VidShare::V0(vid_share))
+                            .context("failed to serialize vid_share")?,
+                    );
+                }
 
                 let vid_common: ADVZCommon = bincode::deserialize(&vid_common_bytes)
                     .context("failed to deserialize vid_common")?;
@@ -1815,10 +1820,15 @@ mod test {
             let mut vid = advz_scheme(2);
             let disperse = vid.disperse(payload.encode()).unwrap();
             let common = disperse.common;
-            let share = disperse.shares[0].clone();
 
             let common_bytes = bincode::serialize(&common).unwrap();
-            let share_bytes = bincode::serialize(&share).unwrap();
+            let share = disperse.shares[0].clone();
+            let mut share_bytes = Some(bincode::serialize(&share).unwrap());
+
+            // insert some nullable vid shares
+            if i % 10 == 0 {
+                share_bytes = None
+            }
 
             tx.upsert(
                 "vid",
